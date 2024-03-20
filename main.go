31677373992a7d6c8730fe98f773e30eeca0f666
shota-imoto <hideyoshi.playing.the.banjo@gmail.com>
2024-03-20 18:09:09 +0900

    ハンマリングなどの2音間のテクニックを表記できる機能

--- main.go
+++ main.go
@@ -30,8 +30,8 @@ func main() {
 }
 
 var SPACE int = 10
-var NOTE_WIDTH int = 14
-var MEASURE_LINE_DEFINE string = "stroke:black;stroke-width:1"
+var NOTE_WIDTH int = 20
+var MEASURE_LINE_DEFINE string = "stroke:#bbb;stroke-width:1"
 
 func drawLine(canvas *svg.SVG) {
 	s := NewScore(Cordinate{200, 200}, 20)
@@ -39,6 +39,7 @@ func drawLine(canvas *svg.SVG) {
 	m1 := l1.addNewMeasure(8, "")
 	m2 := l1.addNewMeasure(8, "")
 	m3 := l1.addNewMeasure(8, "")
+
 	m1.Draw(canvas)
 	m2.Draw(canvas)
 	m3.Draw(canvas)
@@ -46,20 +47,26 @@ func drawLine(canvas *svg.SVG) {
 	MeasureBorder{Measure: m2}.drawStart(canvas)
 	MeasureBorder{Measure: m3}.drawStart(canvas)
 
-	f := Fingering{Measure: m1, Fret: 12, Strings: 3, Beat: 1}
-	f.Draw(canvas)
-	Fingering{Measure: m1, Fret: 4, Strings: 3, Beat: 2}.Draw(canvas)
-	Fingering{Measure: m1, Fret: 12, Strings: 3, Beat: 3}.Draw(canvas)
-	Fingering{Measure: m1, Fret: 4, Strings: 3, Beat: 4}.Draw(canvas)
-	Fingering{Measure: m1, Fret: 12, Strings: 3, Beat: 5}.Draw(canvas)
-	Fingering{Measure: m1, Fret: 12, Strings: 3, Beat: 6}.Draw(canvas)
-	Fingering{Measure: m1, Fret: 12, Strings: 3, Beat: 7}.Draw(canvas)
-	Fingering{Measure: m1, Fret: 12, Strings: 3, Beat: 8}.Draw(canvas)
-	Fingering{Measure: m1, Fret: 12, Strings: 1, Beat: 8}.Draw(canvas)
-	Fingering{Measure: m1, Fret: 12, Strings: 2, Beat: 8}.Draw(canvas)
-	Fingering{Measure: m1, Fret: 12, Strings: 3, Beat: 8}.Draw(canvas)
-	Fingering{Measure: m1, Fret: 12, Strings: 4, Beat: 8}.Draw(canvas)
-	Fingering{Measure: m1, Fret: 12, Strings: 5, Beat: 8}.Draw(canvas)
+	f1, _ := m1.addFingering(2, 3, 1)
+	f1.Draw(canvas)
+	f2, _ := m1.addFingering(3, 3, 2)
+	f2.Draw(canvas)
+
+	Technique{Start: f1, End: f2, Text: "h"}.Draw(canvas)
+
+	f3, _ := m1.addFingering(0, 2, 2)
+	f3.Draw(canvas)
+	f4, _ := m1.addFingering(0, 5, 3)
+	f4.Draw(canvas)
+	f5, _ := m1.addFingering(0, 1, 4)
+	f5.Draw(canvas)
+
+	f6, _ := m2.addFingering(3, 2, 1)
+	f6.Draw(canvas)
+	// f6.DrawCenter(canvas)
+	f7, _ := m2.addFingering(8, 2, 3)
+	f7.Draw(canvas)
+	Technique{Start: f6, End: f7, Text: "s"}.Draw(canvas)
 
 	l2 := s.addNewLine(6, true)
 	m4 := l2.addNewMeasure(6, "break")
@@ -72,9 +79,7 @@ func drawLine(canvas *svg.SVG) {
 	MeasureBorder{Measure: m4}.drawStart(canvas)
 	MeasureBorder{Measure: m5}.drawStart(canvas)
 	MeasureBorder{Measure: m6}.drawStart(canvas)
-	Fingering{Measure: m4, Fret: 4, Strings: 3, Beat: 2}.Draw(canvas)
 
-	// drawMeasure(canvas, Cordinate{100, 100}, 5, 8)
 }
 
 type Cordinate struct {
@@ -133,7 +138,6 @@ func (l *Line) addNewMeasure(beat int, text string) Measure {
 	m := Measure{Base: Cordinate{l.EndX, y}, Strings: l.Strings, Beat: beat, Text: text, withText: l.WithText}
 	l.Measures = append(l.Measures, m)
 	l.EndX = l.EndX + m.Width()
-	fmt.Println(m.Base.Y)
 
 	return m
 }
@@ -179,6 +183,21 @@ func (m Measure) XthStringY(xth int) (int, error) {
 	return m.Base.Y + (xth-1)*SPACE, nil
 }
 
+var FINGERING_CORRECTION_Y int = 5
+
+func (m *Measure) addFingering(fret, strings, beat int) (Fingering, error) {
+
+	x := m.Base.X + (beat-1)*NOTE_WIDTH
+
+	y, err := m.XthStringY(strings)
+	if err != nil {
+		return Fingering{}, err
+	}
+	center_x := x + (NOTE_WIDTH / 2)
+	center_y := y
+	return Fingering{Center: Cordinate{center_x, center_y}, CorrectionY: FINGERING_CORRECTION_Y, Fret: fret, Strings: strings}, nil
+}
+
 type MeasureBorder struct {
 	Measure Measure
 }
@@ -197,20 +216,52 @@ func (b MeasureBorder) drawStart(c *svg.SVG) error {
 
 // 運指。小節に基づいて描画位置が決まる
 type Fingering struct {
-	Measure Measure
-	Fret    int // フレット数
-	Strings int // 何弦
-	Beat    int // 何拍目
+	Center Cordinate
+	Fret   int // フレット数
+
+	// 縦方向の補正。
+	// svgoのText関数はCordinate.Yを底辺として描画するため補正がないと弦の上にフレット番号が乗って表示されてしまう
+	// Fingering.Centerを参照して位置が決まる要素が存在するため、あらかじめCenter.Yに加えるのではなくDraw時に補正する
+	CorrectionY int
+
+	// 以下は、初期化の際にCenterが格納される想定なので正直いらない
+	Strings int // 何弦。
+	Beat    int // 何拍目。
 }
 
-func (f Fingering) Draw(canvas *svg.SVG) error {
-	x := f.Measure.Base.X + (f.Beat-1)*NOTE_WIDTH
-	y, err := f.Measure.XthStringY(f.Strings)
-	if err != nil {
-		return err
+var FINGERING_TEXT_DEFINE string = "text-anchor:middle"
+
+func (f Fingering) Draw(c *svg.SVG) {
+	c.Text(f.Center.X, f.Center.Y+f.CorrectionY, fmt.Sprint(f.Fret), FINGERING_TEXT_DEFINE)
+}
+
+// Fingering動作確認用
+func (f Fingering) DrawCenter(c *svg.SVG) {
+	c.Circle(f.Center.X, f.Center.Y, 2)
+}
+
+type Technique struct {
+	Start Fingering
+	End   Fingering
+	Text  string
+}
+
+var TECHNIQUE_LINE_DEFINE string = "stroke:#444;stroke-width:1.2"
+
+func (t Technique) Draw(c *svg.SVG) error {
+	x := (t.Start.Center.X + t.End.Center.X) / 2
+	if t.Start.Center.Y != t.End.Center.Y {
+		// Center計算時の小数点以下の扱いによってはバグるかも
+		return fmt.Errorf("Technique.Draw is failed: 別の弦なのでおかしい")
 	}
-	corrected_x := x + (NOTE_WIDTH / 2) // text-anchorによって指定した座標を中心に文字列が表示されるため、文字列幅/2を加える
-	corrected_y := y + (SPACE / 2)
-	canvas.Text(corrected_x, corrected_y, fmt.Sprint(f.Fret), "text-anchor:middle")
+	y := t.Start.Center.Y + SPACE
+
+	c.Text(x, y, t.Text, FINGERING_TEXT_DEFINE)
+
+	// start_xとend_xの÷3は補正値。NOTE_WIDTHが極端な値になると文字列と被ったり、文字列から離れすぎたりする
+	start_x := t.Start.Center.X + NOTE_WIDTH/3
+	end_x := t.End.Center.X - NOTE_WIDTH/3
+
+	c.Line(start_x, t.Start.Center.Y, end_x, t.End.Center.Y, TECHNIQUE_LINE_DEFINE)
 	return nil
 }
